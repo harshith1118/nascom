0824aa86f74911e2f95c296b0bf71d5f48bf4d10
Harshith D <dharshith657@gmail.com>
2025-09-19 06:25:04 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -48,15 +48,16 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
           <SidebarMenu>
             {navItems.map((item) => (
               <SidebarMenuItem key={item.href}>
-                <Link href={item.href} legacyBehavior passHref>
-                  <SidebarMenuButton
-                    isActive={pathname === item.href}
-                    tooltip={{ children: item.label }}
-                  >
+                <SidebarMenuButton
+                  asChild
+                  isActive={pathname === item.href}
+                  tooltip={{ children: item.label }}
+                >
+                  <Link href={item.href}>
                     <item.icon />
                     <span>{item.label}</span>
-                  </SidebarMenuButton>
-                </Link>
+                  </Link>
+                </SidebarMenuButton>
               </SidebarMenuItem>
             ))}
           </SidebarMenu>
